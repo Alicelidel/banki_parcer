6a23e6a5de31ba2dcd897738d79b8782f8f8d42b
Alicelidel <kelifriday@yandex.ru>
2017-08-16 19:31:16 +0300

    added function which gets description

---
+++ get_desc.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_desc(url1,url2):
+    """функция получения описания банка"""
+    # получаем ссылку для перехода на страницу банка
+    s = requests.Session()
+    desc = url1 + url2
+    request = s.get(desc)
+    text = request.text
+
+    # получаем ссылку для перехода на описание
+    soup = BeautifulSoup(text, 'lxml')
+    bank_url_to_desc = soup.find('div', {'class': 'layout-columns-wrapper margin-bottom-large'}).find('div', {
+        'class': 'bank-page-header'}).find('a').get('href')
+
+    # переходим на описание и вытаскиваем его
+    # ссылка на описание банка лежит на том же url, но от исходного url надо убрать лишнее, оставив только www.banki.ru
+    desc_url = url1[:-15] + bank_url_to_desc
+
+    # теперь вытаскиваем описание банка по ссылке
+    request = s.get(desc_url)
+    text = request.text
+    soup = BeautifulSoup(text, 'lxml')
+
+    # находим ту часть страницы, в которой хранится описание. описание состоит из блоков em, которых может быть от одного и больше.
+    # если же описания в этих блоках нет, значит у банка нет лицензии, что мы и прописываем далее
+    bank_descs = soup.find('article', {'class': 'font-size-medium font-italic'})
+
+    if bank_descs:
+        bank_descs = bank_descs.find_all('em')
+        bank_desc = ''
+        for elem in bank_descs:
+            bank_desc += elem.text
+        # полученное описание добавляем к банку
+        description = bank_desc
+    else:
+        # в случае отзыва лицензии
+        description = 'Лицензия банка отозвана'
+    # выводим описание банков
+    return description
--- rocket.py
+++ rocket.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from get_desc import get_desc
 
 
 #скачиваем страницу по url
@@ -52,38 +53,5 @@ for result in results:
 
 #получаем описание каждого банка по ссылке
 for bank in results:
-
-    #получаем ссылку для перехода на страницу банка
-    desc = url + bank['bank_url']
-    request = s.get(desc)
-    text = request.text
-
-    #получаем ссылку для перехода на описание
-    soup = BeautifulSoup(text, 'lxml')
-    bank_url_to_desc = soup.find('div', {'class':'layout-columns-wrapper margin-bottom-large'}).find('div', {'class':'bank-page-header'}).find('a').get('href')
-
-    #переходим на описание и вытаскиваем его
-    #ссылка на описание банка лежит на том же url, но от исходного url надо убрать лишнее, оставив только www.banki.ru
-    desc_url = url[:-15] + bank_url_to_desc
-
-    #теперь вытаскиваем описание банка по ссылке
-    request = s.get(desc_url)
-    text = request.text
-    soup = BeautifulSoup(text, 'lxml')
-
-    #находим ту часть страницы, в которой хранится описание. описание состоит из блоков em, которых может быть от одного и больше.
-    #если же описания в этих блоках нет, значит у банка нет лицензии, что мы и прописываем далее
-    bank_descs = soup.find('article', {'class':'font-size-medium font-italic'})
-
-    if bank_descs:
-        bank_descs = bank_descs.find_all('em')
-        bank_desc = ''
-        for elem in bank_descs:
-            bank_desc += elem.text
-        #полученное описание добавляем к банку
-        bank['bank_desc'] = bank_desc
-    else:
-        #в случае отзыва лицензии
-        bank['bank_desc'] = 'Лицензия банка отозвана'
-    #выводим описание банков
-    print(bank['bank_desc'])
+        bank['bank_desc'] = get_desc(url, bank['bank_url'])
+        print(bank['bank_desc'])
